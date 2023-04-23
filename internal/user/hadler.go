5ee078fef17fb01e845c174f4a1ef2c581f19deb
Your Name <ramaro@internet.ru>
2023-04-24 00:11:30 +0300

    First commit

---
+++ internal/user/hadler.go
@@ -0,0 +1,60 @@
+package user
+
+import (
+	"GoLearn/internal/handlers"
+	"github.com/julienschmidt/httprouter"
+	"net/http"
+)
+
+var _ handlers.Handler = &handler{}
+
+const (
+	usersURL = "/users"
+	userURL  = "/users/:uuid"
+)
+
+type handler struct {
+}
+
+func NewHandler() handlers.Handler {
+	return &handler{}
+}
+
+func (h *handler) Register(router *httprouter.Router) {
+	router.GET(usersURL, h.GetList)
+	router.POST(usersURL, h.CreateUser)
+	router.GET(userURL, h.GetUserByUUID)
+	router.PUT(userURL, h.UpdateUser)
+	router.PATCH(userURL, h.PartiallyUpdateUser)
+	router.DELETE(userURL, h.Deleteuser)
+}
+
+func (h *handler) GetList(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	w.WriteHeader(200)
+	w.Write([]byte("this is list of users"))
+}
+
+func (h *handler) CreateUser(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	w.WriteHeader(201)
+	w.Write([]byte("this is Create User"))
+}
+
+func (h *handler) GetUserByUUID(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	w.WriteHeader(200)
+	w.Write([]byte("this is User By UUID"))
+}
+
+func (h *handler) UpdateUser(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	w.WriteHeader(204)
+	w.Write([]byte("this is Update User"))
+}
+
+func (h *handler) PartiallyUpdateUser(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	w.WriteHeader(204)
+	w.Write([]byte("this is Partially Update User "))
+}
+
+func (h *handler) Deleteuser(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	w.WriteHeader(204)
+	w.Write([]byte("this is DELETE of users"))
+}
